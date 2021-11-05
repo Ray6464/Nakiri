d057de63f4a4b52f065415f1de4dee88e6fe4944
Ray Voice <33094591+Ray6464@users.noreply.github.com>
2021-11-05 07:52:00 +0500

    In partially working condition

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # Nakiri
-Create circular JSON objects from .json files using a built-in referencing scheme,
+Create circular JSON objects from .json files using a built-in unix style referencing scheme.
+
+# Usage
+You can import the library using vanilla javascript:
+```
+const nakiri = require('nakiri');
+
+const Jack_Obj = nakiri.parse({
+  name: 'Jack',
+   age: 22,
+ favourite_word: '${{/name}}'
+});
+
+```
+
+# Options
+
+
+Symbolic Legend
+===============
+1. /          = root of Object
+2. ~          = first object below root relative to the current position
+3. EMPTY      = binded to whatever needed in stringify and parse (root is DEFAULT)
+4. .          = current object
+5. ..         = parent object
+6. SYMBOL*    = key of SYMBOL // for v2.0 
+
+
+
+# LICENSE
+MIT License
+
+Copyright (c) 2021 Ray Voice
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
---
+++ app.js
@@ -0,0 +1,124 @@
+// imported modules
+const { readFileSync, writeFileSync, existsSync, lstatSync } = require('fs');
+const { resolve, basename } = require('path'); 
+const flags = require('ray-flags');
+const { sucide } = require('sucide');
+// needs escape sequences for {, }, /, ., ~
+function getNode(_value, _data) {
+  if (_value === '/')   return _data.ROOT;
+  if (_value === '~')   return _data.HOME;
+  if (_value === '..')  return _data.PARENT;
+  if (_value === '.')   return _data.CURRENT;
+  if (_value === SYMLESS_NODE) return _data.SYMLESS;
+  return _value;
+}
+
+function getValue(_path, _data) {
+  const isAbsolute = (_path[0] === '/'); 
+  const isSYMLESS = (_path[0] !== '/' && _path[0] !== '~' && _path[0] !== '..' && _path[0] !== '.');
+
+  if (isSYMLESS === true) _path = SYMLESS_NODE + '/' + _path;
+  const nodes = _path.split(SEPRATOR).map(_node => getNode(_node, _data));
+
+  if (isAbsolute === true) nodes.unshift('/');
+  const value = nodes.reduce((_prev_node_name, _node_name) => _prev_node_name[_node_name]);
+  return value;
+}
+
+const SEPRATOR = '/'; // get form flags or args in v2.0
+const SYMLESS_NODE = "~/info"; // get from flags or args nowi, add a machanism to remove '/' from the end of the string if there is one or more of them there
+const sampleObject = {
+  david: {
+    info: {
+      name: "David",
+      age: 22,
+      sex: 'male',
+    },
+    employement: {
+      title: "Jr. Software Tester",
+      pay: "60k",
+//      bonus: "${{(function christmassBonus() { return (+'$((/david/info/age')) / 2) + 'k' }}})()", // not working, will implement in v2.0
+      detailes: {
+        name: "${{~/info/name}}",
+        age: "${{~/info/age}} years old ${{~/info/sex}}",
+        sex: "${{sex}}"
+      }
+    }
+  }
+}
+
+const ELEMENTS_JSON = sampleObject; // get from flags or args now
+const JSON_DATA_STRING = JSON.stringify(ELEMENTS_JSON);
+const REFERENCES = mapObjectRecursively(0, ELEMENTS_JSON, _key => _key, (_value, _meta_data) => { // The data argument must have {_root, _home, _parent, _symless}
+  const REFERENCE_ITEMS = [_value, ...(JSON.stringify(_value).match(/\$\{\{[^\O\}]+\}\}/g) || [])
+  .map(MATCH => (
+    {
+      REFERENCE: MATCH,
+      SUB_REFERENCES: MATCH.match(/\$\(\([^\O\}]+\)\)/g),
+    }
+  )).map(MATCH => (
+    {
+      REFERENCE: {
+        REF_STR: _value,
+	REF: MATCH.REFERENCE,
+        REF_PATH: MATCH.REFERENCE.replace(/^(\$\{\{)/, '').replace(/(\}\})$/, '')
+      },
+      SUB_REFERENCES: (MATCH.SUB_REFERENCES || []).map(ref => ({
+        REF_STR: ref,
+        REF_PATH: ref.replace(/^(\$\(\()/, '').replace(/(\)\))$/, '')
+      }))
+    }
+  )).map(MATCH => (
+    {
+      ...MATCH,
+      REFERENCE: {
+        ...MATCH.REFERENCE,
+        REF_VALUE: getValue(MATCH.REFERENCE.REF_PATH, _meta_data),
+      }
+      //ROOT: _meta_data.ROOT, //HOME: _meta_data.HOME, //PARENT: _meta_data.PARENT, //CURRENT: _meta_data.CURRENT, //SYMLESS: _meta_data.SYMLESS,
+    }
+  ))].reduce((prev, curr) => prev.replace(curr.REFERENCE.REF, curr.REFERENCE.REF_VALUE));//.map(MATCH => _value/*MATCH.REFERENCE.REF_VALUE*/);
+  return REFERENCE_ITEMS;
+}, {
+  ROOT: ELEMENTS_JSON,
+  HOME: ELEMENTS_JSON,
+  PARENT: null,
+  CURRENT: ELEMENTS_JSON,
+  SYMLESS: SYMLESS_NODE 
+});
+
+//console.log( JSON.stringify(sampleObject, null, 2) );
+console.log( JSON.stringify(REFERENCES, null, 2) );
+//console.log( REFERENCES[1].SUB_REFERENCES );
+
+function mapObjectRecursively(_depth_count, _obj, _keyModFunction, _valueModFunction, _meta_data) { // _meta_hive is data required at all levels of a recursive iteration
+  if (Array.isArray(_obj)) return _obj.map(_i => mapObjectRecursively(_depth_count+1, _i, _keyModFunction, _valueModFunction, {
+    ROOT: _meta_data.ROOT,
+    HOME: (_depth_count === 1 ? _obj : _meta_data.HOME),
+    PARENT: _obj,
+    CURRENT: _i,
+    SYMLESS: _meta_data.SYMLESS 
+  }));
+  if (typeof(_obj) === 'object' && _obj !== null) {
+	  const ENTRIES = Object.entries(_obj).map(_entry => [_keyModFunction(_entry[0]), mapObjectRecursively(_depth_count+1, _entry[1], _keyModFunction, _valueModFunction, {
+            ROOT: _meta_data.ROOT,
+            HOME: (_depth_count === 1 ? _obj : _meta_data.HOME),
+            PARENT: _obj,
+            CURRENT: _entry[1],
+            SYMLESS: _meta_data.SYMLESS 
+	  })]);
+	  return Object.fromEntries(ENTRIES);
+  }
+  return _valueModFunction(_obj, _meta_data); // when object is STRING, NUMBER, BOOLEAN, etc.
+};
+
+module.export = {
+  stringify: function(_JSON, SYMLESS, BREAK_POINT_DEPTH) {
+    const output = {};
+    return output;
+  },
+  parse: function(_STRING, SYMLESS) {
+    return "method under construction";
+  },
+}
+
